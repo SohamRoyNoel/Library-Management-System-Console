f1aa835a54203d505d74069dca4b412d8f133b29
Backend Ninja <36095087+SohamRoyNoel@users.noreply.github.com>
2026-02-10 16:18:36 +0530

    Adding mappings on booking

--- src/main/java/com/lms/modules/borrowings/PrintBorrowingsTable.java
+++ src/main/java/com/lms/modules/borrowings/PrintBorrowingsTable.java
@@ -12,41 +12,17 @@ public class PrintBorrowingsTable {
     public static void printBorrowingsTable(List<Borrowing> borrowings) {
         TablePrinter.print(borrowings, List.of(
                 new ColumnDef<>("ID", 5, b -> String.valueOf(b.getId())),
-
-                new ColumnDef<>("BORROWED AT", 12,
-                        b -> DF.format(b.getBorrowedAt())),
-
-                new ColumnDef<>("BORROWED TILL", 12,
-                        b -> DF.format(b.getBorrowedTill())),
-
-                new ColumnDef<>("QTY", 5,
-                        b -> String.valueOf(b.getQuantity())),
-
-                new ColumnDef<>("RETURNED", 9,
-                        b -> String.valueOf(b.getIsReturned())),
-
-                new ColumnDef<>("RETURNED AT", 12,
-                        b -> b.getReturnedAt() != null
-                                ? DF.format(b.getReturnedAt())
-                                : "-"),
-
-                new ColumnDef<>("BOOK ISBN", 15,
-                        b -> b.getBook().getIsbn()),
-
-                new ColumnDef<>("BOOK NAME", 20,
-                        b -> b.getBook().getTitle()),
-
-                new ColumnDef<>("MEMBER ID", 12,
-                        b -> b.getMember().getMembershipVirtualId()),
-
-                new ColumnDef<>("MEMBER NAME", 15,
-                        b -> b.getMember().getName()),
-
-                new ColumnDef<>("EMAIL", 22,
-                        b -> b.getMember().getEmail()),
-
-                new ColumnDef<>("PHONE", 12,
-                        b -> b.getMember().getPhoneNumber())
+                new ColumnDef<>("BORROWED AT", 10, b -> DF.format(b.getBorrowedAt())),
+                new ColumnDef<>("BORROWED TILL", 5, b -> DF.format(b.getBorrowedTill())),
+                new ColumnDef<>("QUANTITY", 5, b -> String.valueOf(b.getQuantity())),
+                new ColumnDef<>("IS RETURNED", 5, b -> String.valueOf(b.getIsReturned())),
+                new ColumnDef<>("RETURNED AT", 5, b -> b.getReturnedAt() != null ? DF.format(b.getReturnedAt()) : null),
+                new ColumnDef<>("BOOK ISBN", 8, b -> b.getBook().getIsbn()),
+                new ColumnDef<>("BOOK NAME", 5, b -> b.getBook().getTitle()),
+                new ColumnDef<>("MEMBER ID", 5, b -> b.getMember().getMembershipVirtualId()),
+                new ColumnDef<>("MEMBER NAME", 5, b -> b.getMember().getName()),
+                new ColumnDef<>("MEMBER EMAIL", 5, b -> b.getMember().getEmail()),
+                new ColumnDef<>("MEMBER PHONE", 5, b -> b.getMember().getPhoneNumber())
         ));
     }
 }
